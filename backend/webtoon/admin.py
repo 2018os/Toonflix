f2f4886414358833c3b445ce6e07bb0a39756a88
김승엽 <seungyeop-kim@gimseung-yeobs-MacBook-Pro.local>
2020-05-13 01:42:39 +0900

    Implement admin page

--- backend/webtoon/admin.py
+++ backend/webtoon/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Webtoon, Author
+from .models import Theme, Genre, Webtoon, Author
 
 admin.site.register(Webtoon)
 admin.site.register(Author)
+admin.site.register(Theme)
+admin.site.register(Genre)
