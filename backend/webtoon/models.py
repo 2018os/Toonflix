48f25e0a2ef31adfb9d5866a48a3304bcdf08390
김승엽 <seungyeop-kim@gimseung-yeobs-MacBook-Pro.local>
2020-05-13 01:16:23 +0900

    Add Genre, Theme model

--- backend/webtoon/models.py
+++ backend/webtoon/models.py
@@ -2,6 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+class Genre(models.Model):
+	name = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.name
+
+
+class Theme(models.Model):
+	title = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.title
+
+
 class Webtoon(models.Model):
 	PLATFORM_CHOICES = (
 		# ('db', 'form')
@@ -10,13 +24,13 @@ class Webtoon(models.Model):
 		('L', 'Lezhin')
 	)
 	title = models.CharField(max_length=100)
-	genre = models.CharField(max_length=50) # n:n
-	author = models.CharField(max_length=100) # n:n
+	genre = models.ManyToManyField(Genre)
+	author = models.ManyToManyField('Author')
 	is_finish = models.BooleanField()
 	is_adult = models.BooleanField()
 	is_free = models.BooleanField()
-	theme = models.CharField(max_length=200, null=True) # n:n
-	platform = models.CharField(max_length=1, choices=PLATFORM_CHOICES) # choice
+	theme = models.ManyToManyField(Theme, null=True)
+	platform = models.CharField(max_length=1, choices=PLATFORM_CHOICES)
 	thumbnail = models.URLField(max_length=300)
 
 	# need testing
