e6787c7facb8da36f28ba1a18188af2a73b649e4
김승엽 <seungyeop-kim@gimseung-yeobs-MacBook-Pro.local>
2020-05-21 19:58:56 +0900

    Add <WebtoonList />

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import WebtoonList from './WebtoonList';
 
-import Loading from './Loading';
-import Error from './Error';
-import WebtoonCard from './WebtoonCard';
-
-const App = () => {
-  const [data, setData] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
-      try {
-        const result = await axios('http://127.0.0.1:8000/api/');
-        setData(result.data);
-        setIsLoading(false);
-      } catch(err) {
-        setIsError(true);
-      }
-    };
-    fetchData();
-  }, []);
-  return (
-    <div className="App">
-      {
-        isError
-        ? <Error />
-        : (
-          !isLoading && data
-          ? <WebtoonCard data={data} />
-          : <Loading />
-        )
-      }
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    {/* TODO: Add <Navigation />
+    TODO: Add <TopBanner /> */}
+    <WebtoonList />
+    {/* TODO: Add <Footer /> */}
+  </div>
+)
 
 export default App;
---
+++ frontend/src/WebtoonList.jsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import { Card, Carousel } from 'antd';
+import 'antd/dist/antd.css';
+
+import Error from './Error';
+import Loading from './Loading';
+import WebtoonCard from './WebtoonCard';
+
+const settings = {
+	dots: false,
+	infinite: true,
+	slidesToShow: 4,
+	slidesToScroll: 4
+}
+
+const WebtoonList = () => {
+	const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+	const [isError, setIsError] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+				const result = await axios('http://127.0.0.1:8000/api/genre/4');
+        setData(result.data);
+        setIsLoading(false);
+      } catch(err) {
+        setIsError(true);
+      }
+    };
+    fetchData();
+	}, []);
+	const slider = useRef();
+	const onClickNext = () => {
+		slider.current.next();
+	};
+	const onClickPrev = () => {
+		slider.current.prev();
+	};
+
+  return (
+    <div className="App">
+      {
+        isError
+        ? <Error />
+        : (
+          !isLoading && data
+          ? (
+						<div>
+							<strong>#{data.name}</strong>
+							<button onClick={onClickPrev}>←</button>
+							<button onClick={onClickNext}>→</button>
+							<Carousel
+								{...settings}
+								ref={ref => {
+									slider.current = ref
+								}}
+							>
+								{
+									data.webtoons.map(webtoon => (
+										<Card
+											cover={<img src={webtoon.thumbnail} alt="thumbnail" />}
+											key={webtoon.id}
+										>
+											<Card.Meta
+												title={webtoon.title}
+												description={webtoon.authors}
+											/>
+										</Card>
+									))
+								}
+							</Carousel>
+						</div>
+					)
+          : <Loading />
+        )
+      }
+    </div>
+  );
+};
+
+export default WebtoonList;
