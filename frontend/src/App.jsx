13ea1a97b0139c17fbb52ff6ce34d5fb589c9188
김승엽 <seungyeop-kim@gimseung-yeobs-MacBook-Pro.local>
2020-05-23 17:19:14 +0900

    Add <Main />

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,49 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import WebtoonList from './components/WebtoonList';
-import Container from './layout/Container';
+
+import Main from './components/Main';
 import theme from './theme';
 import './App.less';
 
+
+// For checking layout
+
 import styled from 'styled-components';
+import Container from './layout/Container';
+import Background from './layout/Background';
 
 const Div = styled.div`
   margin-top: 10px;
   background: #5E74FF;
   height: 50px;
 `;
+
+// Will DELETE
+
 const App = () => (
   <ThemeProvider theme={theme}>
-    {/* TODO: Add <Navigation />
-    TODO: Add <TopBanner /> */}
-    <Container>
-      <Div />
-    </Container>
-    {/* <Container> */}
-      <WebtoonList />
-    {/* </Container> */}
-    <Container>
-      <Div />
-    </Container>
-    {/* TODO: Add <Footer /> */}
+    <Background color="#EAEDED">
+
+      {/*
+      TODO: Add <Navigation />
+      TODO: Add <TopBanner />
+      */}
+
+      <Container>
+        <Div />
+      </Container>
+
+      <Main />
+
+      <Container>
+        <Div />
+      </Container>
+
+      {/*
+      TODO: Add <Footer />
+      */}
+
+    </Background>
   </ThemeProvider>
 )
 
---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import WebtoonList from './WebtoonList';
+import Container from '../layout/Container';
+
+const Main = () => (
+  <Container>
+    <WebtoonList />
+  </Container>
+);
+
+export default Main;
