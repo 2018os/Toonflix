d111666f2bd46793386df4fc9af58aa7ddd08b0f
김승엽 <seungyeop-kim@gimseung-yeobs-MacBook-Pro.local>
2020-05-26 02:20:20 +0900

    Remove <Div />

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import { ThemeProvider, createGlobalStyle } from 'styled-components';
 
-import Main from './components/Main';
 import theme from './theme';
 import './styles/globals';
-
-// For checking layout
-
-import styled from 'styled-components';
-import Container from './layout/Container';
 import Background from './layout/Background';
 
-const Div = styled.div`
-  margin-top: 10px;
-  background: #5E74FF;
-  height: 50px;
-`;
-
-// Will DELETE
+import Main from './components/Main';
 
 const GlobalStyle = createGlobalStyle`
 /* http://meyerweb.com/eric/tools/css/reset/ 
@@ -79,17 +67,7 @@ const App = () => (
       TODO: Add <Navigation />
       TODO: Add <TopBanner />
       */}
-
-      <Container>
-        <Div />
-      </Container>
-
       <Main />
-
-      <Container>
-        <Div />
-      </Container>
-
       {/*
       TODO: Add <Footer />
       */}
