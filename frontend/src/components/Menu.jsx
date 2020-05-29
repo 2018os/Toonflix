2249e131fe18bef6ddd2f96f589bf2c4657fd32a
김승엽 <seungyeop-kim@gimseung-yeobs-MacBook-Pro.local>
2020-05-29 22:13:43 +0900

    Fix buttons layout in <Menu />

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 
 const ButtonWrapper = styled.div`
   text-align: center;
+  margin-left: ${props => props.theme.spacing[2]}; // TODO: Make no sense
 `;
 
 const StyledButton = styled(Button)`
