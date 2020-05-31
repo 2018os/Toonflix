4fac6dc300cba53ea226507becf87bc773eb8361
김승엽 <seungyeop-kim@gimseung-yeobs-MacBook-Pro.local>
2020-05-31 17:27:13 +0900

    Add <withFetch /> for fetching data

---
+++ frontend/src/hocs/withFetch.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect, Component } from 'react';
+import axios from 'axios';
+
+const withFetch = fetchUrl => WrappedComponent => {
+  return props => {
+    const [data, setData] = useState();
+    const [isError, setIsError] = useState(false);
+    useEffect(() => {
+      const fetch = async () => {
+        try {
+          const result = await axios(fetchUrl);
+          setData(result.data);
+        } catch(err) {
+          setIsError(true);
+        }
+      };
+      fetch();
+    }, []);
+    return <WrappedComponent {...props} data={data} isError={isError} />
+  }
+};
+
+export default withFetch;
