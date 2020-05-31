13d2148a68426410c4648046ccb58813ba0fd8c7
김승엽 <seungyeop-kim@gimseung-yeobs-MacBook-Pro.local>
2020-06-01 04:07:33 +0900

    Add filters for Search

--- backend/webtoon/views.py
+++ backend/webtoon/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 from .models import Theme, Webtoon, Genre, Author
 from .serializers import ThemeSerializer, WebtoonSerializer, GenreSerializer, AuthorSerializer
 
 class ThemeViewSet(viewsets.ReadOnlyModelViewSet):
+	search_fields = ['webtoons__title']
+	filter_backends = (filters.SearchFilter,)
 	queryset = Theme.objects.all()
 	serializer_class = ThemeSerializer
 
 
 class WebtoonViewSet(viewsets.ReadOnlyModelViewSet):
+	search_fields = ['title']
+	filter_backends = (filters.SearchFilter,)
 	queryset = Webtoon.objects.all()
 	serializer_class = WebtoonSerializer
 
