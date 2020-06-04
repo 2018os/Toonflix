539dd77cb33d8471d9a153ea3c07173c0d311b63
김승엽 <seungyeop-kim@gimseung-yeobs-MacBook-Pro.local>
2020-06-04 15:46:27 +0900

    Initialize <Footer />

--- frontend/src/layout/Footer.jsx
+++ frontend/src/layout/Footer.jsx
@@ -1,16 +1,22 @@
+import { Margin } from 'styled-components-spacing';
 import React from 'react';
+import { Space } from 'antd';
 
 // layout
 import Container from './Container';
 
+// styles
+import { Text } from '../styles/Typography';
+
 const Footer = () => (
   <Container>
-    <div
-      style={{
-        height: '111px',
-        backgroundColor: 'black'
-      }}
-    />
+    <Margin vertical={4}>
+      <Space>
+        <Text size="largest" bold>
+          TOONFLIX
+        </Text>
+      </Space>
+    </Margin>
   </Container>
 );
 
