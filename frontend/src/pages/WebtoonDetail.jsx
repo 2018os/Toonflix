a723a8645817c9e9b974c1b9406fd2ba1d7fe51f
김승엽 <seungyeop-kim@gimseung-yeobs-MacBook-Pro.local>
2020-06-06 18:31:24 +0900

    Apply withFetchParams in <WebtoonDetail />

--- frontend/src/pages/WebtoonDetail.jsx
+++ frontend/src/pages/WebtoonDetail.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
 
-const WebtoonDetail = () => (
-  <div>
-    WebtoonDetail
-  </div>
-);
+// hocs
+import withFetchParams from 'hocs/withFetchParams';
 
-export default WebtoonDetail;
+// layout
+import Container from 'layout/Container';
+import { Page } from 'layout/Layout';
+
+// components
+import Thumbnail from 'components/Thumbnail';
+
+const WebtoonDetail = ({ data: webtoon }) => {
+  const {
+    thumbnail,
+    is_pay,
+    is_adult,
+    is_finish,
+    platform,
+  } = webtoon || '';
+
+  const widget = {
+    pay: is_pay,
+    adult: is_adult,
+    finish: is_finish,
+    platform,
+  };
+
+  return (
+    <Page background="gray">
+      <Container>
+        {
+          webtoon
+          ? (
+            <Thumbnail src={thumbnail} widget={widget} />
+          )
+          : 'loading...'
+        }
+      </Container>
+    </Page>
+  )
+};
+
+export default withFetchParams('http://127.0.0.1:8000/api/webtoon')(WebtoonDetail);
