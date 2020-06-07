43b05bc4cfbeef9baeab905ff94c6b1e0036935d
김승엽 <seungyeop-kim@gimseung-yeobs-MacBook-Pro.local>
2020-06-07 17:14:03 +0900

    Add <withNavigation />

--- frontend/src/layout/Navigation.jsx
+++ frontend/src/layout/Navigation.jsx
@@ -1,55 +1,50 @@
+import { Input, Space } from 'antd';
+import { Link } from 'react-router-dom';
+import { Margin, Padding } from 'styled-components-spacing';
 import React from 'react';
-import { Padding } from 'styled-components-spacing';
-import { Row, Col, Space, Input } from 'antd';
-import styled from 'styled-components';
 
-import { Text } from '../styles/Typography';
-import Icon from '../styles/Icon';
+// layout
+import Container from './Container';
+
+// styles
+import { Text } from 'styles/Typography';
 
 const Logo = () => (
-  <div>
-    Logo
+  <div style={{
+    position: 'absolute'
+  }}>
+    <Link to="/">
+      Home Logo
+    </Link>
   </div>
 );
 
-const MenuCol = styled(Col)`
-  margin-left: ${props => props.theme.spacing[1]};
-`;
-
-const ProfileCol = styled(Col)`
-  text-align: right;
-`;
-
 const Menu = () => (
   <Space size="large">
-    <a href="/themes">
-      <Text size="large">
-        컬렉션
-      </Text>
-    </a>
-    <a href="/genres">
-      <Text size="large">
-        카테고리
-      </Text>
-    </a>
+    <Margin right={2}>
+      <Link to="/collections">
+        <Text size="large">
+          컬렉션
+        </Text>
+      </Link>
+    </Margin>
+    <Margin right={3}>
+      <Link to="/category">
+        <Text size="large">
+          카테고리
+        </Text>
+      </Link>
+    </Margin>
     <Input placeholder="제목, 장르, 작가를 입력하세요." />
   </Space>
 );
 
-const Profile = () => (
-  <Space size="large">
-    <Icon src="/icon/star.svg" />
-    <Icon src="/icon/account-circle.svg" />
-  </Space>
-);
-
 const Navigation = () => (
-  <Padding horizontal={4} vertical={2}>
-    <Row justify="space-around">
-      <Col span={4}><Logo /></Col>
-      <MenuCol span={14}><Menu /></MenuCol>
-      <ProfileCol span={4}><Profile /></ProfileCol>
-    </Row>
+  <Padding all={2}>
+    <Logo />
+    <Container>
+      <Menu />
+    </Container>
   </Padding>
 );
 
