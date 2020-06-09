919a236b0dba0fe8ed228d8f464177bb55898ae5
김승엽 <seungyeop-kim@gimseung-yeobs-MacBook-Pro.local>
2020-06-09 13:49:19 +0900

    Add <Icon />

--- frontend/src/styles/Icon.jsx
+++ frontend/src/styles/Icon.jsx
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
 
 const Icon = styled.img`
-
+  ${props => `
+    width: ${props.theme.iconSizes[props.size]};
+    height: ${props.theme.iconSizes[props.size]};
+  `}
 `;
+
+Icon.defaultProps = {
+  size: 'default',
+}
 
 export default Icon;
