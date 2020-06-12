0d0ac5614f23016b5a89a3cfb4970079bc7de9c6
seungyeop Kim <seungyeop2002@gmail.com>
2020-06-13 03:30:25 +0900

    Implement setState logic in <withFetchAll />

--- frontend/src/hocs/withFetchAll.jsx
+++ frontend/src/hocs/withFetchAll.jsx
@@ -5,15 +5,18 @@ import React, { useEffect, useState } from 'react';
 const withFetchAll = fetchUrls => WrappedComponent => {
   return props => {
     const requestList = [];
-    const [data, setData] = useState();
+    const dataList = []; // result data list
+    const [data, setData] = useState([]);
     const [isError, setIsError] = useState(false);
     fetchUrls.map(url => requestList.push(axios.get(url)));
     useEffect(() => {
       const fetch = async () => {
         try {
           const result = await axios.all(requestList);
-          console.log(result);
-          // setData(result.data);
+          result.map(r => {
+            dataList.push(r.data);
+          });
+          setData(dataList);
         } catch(err) {
           setIsError(true);
         }
