af305e2a6b64f91229149e4410eadb28f1f583f0
seungyeop Kim <seungyeop2002@gmail.com>
2020-06-13 04:22:32 +0900

    Change Slider title in <ThemeList />

--- frontend/src/components/ThemeList.jsx
+++ frontend/src/components/ThemeList.jsx
@@ -6,7 +6,7 @@ import ThemeCard from './ThemeCard';
 
 const ThemeList = ({ data: themes }) => (
   themes && (
-    <Slider title="비슷한 작품" slidesToShow={3}>
+    <Slider title="작품이 포함된 컬렉션" slidesToShow={3}>
       {
         themes.map(theme => (
           <ThemeCard theme={theme} key={`theme-card-${theme.id}`} />
