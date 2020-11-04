88200a1fc17a9e30142e0ba4bc3a659b7ff0b5a1
seungyeop Kim <seungyeop2002@gmail.com>
2020-11-04 14:39:48 +0900

    Add props generic type on higher order components

--- src/hocs/withFooter.tsx
+++ src/hocs/withFooter.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Footer from '../layout/footer';
 
-const withFooter = (WrappedComponents: React.ComponentType) => {
+const withFooter = (WrappedComponents: React.ComponentType<any | string>) => {
   return (props: any) => {
     return (
       <>
--- src/hocs/withNavigation.tsx
+++ src/hocs/withNavigation.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 import Navigation from '../layout/Navigation';
 
-const withNavigation = (WrappedComponents: React.ComponentType) => {
+const withNavigation = (
+  WrappedComponents: React.ComponentType<any | string>
+) => {
   return (props: any) => {
     return (
       <>
