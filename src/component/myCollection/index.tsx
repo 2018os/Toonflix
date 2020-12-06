6c135d8b0d1aa2dc5cb2b96a6f9cee50c3fb44cd
seungyeop Kim <seungyeop2002@gmail.com>
2020-12-06 22:36:22 +0900

    Enhance Button design on myCollection page

--- src/component/myCollection/index.tsx
+++ src/component/myCollection/index.tsx
@@ -1,8 +1,11 @@
 import React, { FunctionComponent, useState } from 'react';
+import styled from 'styled-components';
 
 import withAuth, { AuthState } from '../../hocs/withAuth';
 
 import CollectionCardList from './CollectionCardList';
+
+import { spacing, Colors } from '../../util/theme';
 
 export interface Props {
   authState: AuthState;
@@ -10,28 +13,56 @@ export interface Props {
 
 type CollectionType = 'myCollections' | 'likedCollections';
 
+type ButtonProps = {
+  isSelected: boolean;
+};
+
+const CollectionButtonWrapper = styled.div`
+  margin-bottom: ${spacing[3]};
+`;
+
+const CollectionButton = styled.button<ButtonProps>`
+  width: 160px;
+  padding: ${spacing[1]};
+  border-width: 0;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  background-color: ${(props) =>
+    props.isSelected ? Colors.PRIMARY_COLOR : Colors.WHITE};
+  color: ${(props) => (props.isSelected ? Colors.WHITE : Colors.BLACK)};
+  &:first-child {
+    border-radius: 8px 0 0 8px;
+  }
+  &:last-child {
+    border-radius: 0 8px 8px 0;
+  }
+`;
+
 const MyCollectionContainer: FunctionComponent<Props> = ({ authState }) => {
   const [collectionType, setCollectionType] = useState<CollectionType>(
-    'likedCollections'
+    'myCollections'
   );
   return (
-    <div>
-      <button
-        type="button"
-        onClick={() => {
-          setCollectionType('myCollections');
-        }}
-      >
-        내꺼
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          setCollectionType('likedCollections');
-        }}
-      >
-        좋아
-      </button>
+    <>
+      <CollectionButtonWrapper>
+        <CollectionButton
+          type="button"
+          isSelected={collectionType === 'myCollections'}
+          onClick={() => {
+            setCollectionType('myCollections');
+          }}
+        >
+          내꺼
+        </CollectionButton>
+        <CollectionButton
+          type="button"
+          isSelected={collectionType === 'likedCollections'}
+          onClick={() => {
+            setCollectionType('likedCollections');
+          }}
+        >
+          좋아
+        </CollectionButton>
+      </CollectionButtonWrapper>
       {authState.me ? (
         <CollectionCardList
           data={authState.me?.[collectionType]}
@@ -42,7 +73,7 @@ const MyCollectionContainer: FunctionComponent<Props> = ({ authState }) => {
       ) : (
         <div>Loading</div>
       )}
-    </div>
+    </>
   );
 };
 
