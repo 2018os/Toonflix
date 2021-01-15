1ea6740a27e174841792da9494a9570abdcf1e2b
seungyeop Kim <seungyeop2002@gmail.com>
2021-01-15 20:15:40 +0900

    Set 'width: 992px'

--- src/layout/Container.ts
+++ src/layout/Container.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  width: 1024px; // for Desktop breakpoint
+  // width: 1024px; // for Desktop breakpoint
+  width: 992px; // for Desktop breakpoint
   // width: 768px; // for Tablet breakpoint
   margin: auto;
 `;
